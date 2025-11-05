60607ece2ed875dea088aeab6d8c31921195eec2
Mythic <Alemsm1@gmail.com>
2025-11-05 09:28:57 -0500

    Tentative fix for "property undefined" when responding to return of playback state.

--- public/playback.js
+++ public/playback.js
@@ -44,7 +44,7 @@ window.onload = async () => {
 };
 
 window.onReturnPlaybackState = (ps) => {
-    updateNowPlaying("music", ps.music.playlist_name, ps.music.track_name);
+    updateNowPlaying("music", ps.get("music").playlist_name, ps.get("music").track_name);
     updateNowPlaying("ambience", "Ambience", ps.ambience.name);
     updateVCButtons();
 }
